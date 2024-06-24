0ebf50908fe9384f109ae92ffd1a62cd386a281c
Bonaparte003 <b.uteramaho@alustudent.com>
2024-06-24 01:24:14 -0700

    updates

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __main__ == "__name__":
+if __name__ == "__main__":
     from sys import argv
     total = 0
     a = len(argv)
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __main__ == "__name__":
+if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
         if '__' not in i:
