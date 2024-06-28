49beaf10e67f0806b1d4a89149815b02b505a1c8
Bonaparte003 <b.uteramaho@alustudent.com>
2024-06-28 10:20:56 +0200

    done

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    num = roman_string
+    if not isinstance(num, str) or num is None:
         return 0
-    
+
     # Dictionary to map Roman numerals to integers
     roman_dict = {
         'I': 1,
@@ -13,16 +14,16 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    
+
     # Initialize the integer result
     result = 0
-    
+
     # Iterate over the Roman numeral string
-    for i in range(len(roman_string)):
+    for i in range(len(num)):
         # If the current value is less than the next value, subtract it
-        if i + 1 < len(roman_string) and roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
-            result -= roman_dict[roman_string[i]]
+        if i + 1 < len(num) and roman_dict[num[i]] < roman_dict[num[i + 1]]:
+            result -= roman_dict[num[i]]
         else:
-            result += roman_dict[roman_string[i]]
-    
+            result += roman_dict[num[i]]
+
     return result
