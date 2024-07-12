0274880b0cdd9d6cb40b1f669baf1d2a36b336a6
Bonaparte <b.uteramaho@alustudent.com>
2024-07-12 09:11:00 +0200

    task_2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_same_class method."""
+
+
+def is_same_class(obj, a_class):
+    """checks if an object is an instance of a class."""
+    return(isinstance(obj, a_class))
