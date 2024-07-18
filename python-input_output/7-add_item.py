401d370f38e593fe4827e2d8fdd7ea450f58ffc6
Bonaparte <b.uteramaho@alustudent.com>
2024-07-18 10:24:06 +0200

    updates

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """write script that adds to python list"""
-
 from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-filename = "add_item.json"
 
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
 
-for i in argv[1:]:
-    json_list.append(argv)
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    filename = "add_item.json"
 
-save_to_json_file(json_list, filename)
+    try:
+        json_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        json_list = []
+
+    for i in argv[1:]:
+        json_list.append(argv)
+
+    save_to_json_file(json_list, filename)
