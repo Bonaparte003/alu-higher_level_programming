b717aae21db26d1b21749079974fa9367b331c04
Bonaparte <b.uteramaho@alustudent.com>
2024-07-18 10:28:52 +0200

    updates

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""write script that adds to python list"""
-from sys import argv
+"""module that adds arguments to a list and saves it to a file"""
 
+
+import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-    filename = "add_item.json"
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
     try:
-        json_list = load_from_json_file(filename)
+        loadFile = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        json_list = []
+        loadFile = []
 
-    for i in argv[1:]:
-        json_list.append(argv)
-
-    save_to_json_file(json_list, filename)
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
