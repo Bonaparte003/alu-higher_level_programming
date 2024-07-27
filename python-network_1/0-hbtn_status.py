c64020dd8d12a5bcbb33cbb3b6e8225792aae0f1
Bonaparte003 <b.uteramaho@alustudent.com>
2024-07-27 16:39:19 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-A python script that fetches data from a url
-displays the type(binary), raw_content and
-decoded content in utf-8
+""" documentation
+- Fetches data from https://alu-intranet.hbtn.io/status
+- uses urrlib module
 """
 
 import urllib.request
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+url = "https://alu-intranet.hbtn.io/status"
+if __name__ == '__main__':
+    with urllib.request.urlopen(url) as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
