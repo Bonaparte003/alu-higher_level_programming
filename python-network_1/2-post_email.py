10c344af22d45298c09c35a1cab4dbe26fea217a
Bonaparte <b.uteramaho@alustudent.com>
2024-07-29 09:33:48 +0200

    posting with an email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -9,11 +9,10 @@ import urllib.request
 import urllib.parse
 
 if __name__ == '__main__':
-	"""START"""
-	email = sys.argv[2]
-	url = sys.argv[1]
-
-	data = urllib.parse.urlencode({'email':email}).encode("utf-8")
-	request = urllib.request.Request(url, data=data, method="POST")
-	with urllib.request.urlopen(request) as f:
-		print(f.read().decode("utf-8"))
+    """START"""
+    email = sys.argv[2]
+    url = sys.argv[1]
+    data = urllib.parse.urlencode({'email': email}).encode("utf-8")
+    request = urllib.request.Request(url, data=data, method="POST")
+    with urllib.request.urlopen(request) as f:
+        print(f.read().decode("utf-8"))
