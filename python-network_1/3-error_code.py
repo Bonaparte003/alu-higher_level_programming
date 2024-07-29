1456fe373fdcb1ff15c96fce2bc2997fbed6e92a
Bonaparte <b.uteramaho@alustudent.com>
2024-07-29 10:12:58 +0200

    3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-takes URL sends a request to the URL
-Displays the body of the response
+takes a URL and sends a request to the url
+handles errors
 """
 
-import sys
-import urllib.error
 import urllib.request
+import urllib.error
+import sys
 
-if __name__ == "__main__":
-    """handles errors"""
+if __name__ == '__main__':
+    """"Documented"""
     url = sys.argv[1]
-    request = urllib.request.Request(url)
+    reqq = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(request) as f:
-            print(f.read().decode("utf-8"))
+        with urllib.request.urlopen(reqq) as response:
+            content = response.read()
+            print("{}".format(content.decode("utf-8")))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
-    except urllib.error.URLError as n:
-        print(n.reason)
+    except urllib.error.URLError as e:
+        print(e.reason)
