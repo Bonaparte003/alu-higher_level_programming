15be936ccf8ca179dc44cc7308fe17fd65661b18
Bonaparte003 <b.uteramaho@alustudent.com>
2024-07-29 01:58:30 -0700

    new_updates

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Fetches url"""
+"""Fetches url
+using the request moduel
+"""
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
+    url = 'https://intranet.hbtn.io/status'
     if url.startswith('https://'):
-        url = "https://alx-intranet.hbtn.io/status"
-    req = requests.get(url)
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format((req.text)))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
