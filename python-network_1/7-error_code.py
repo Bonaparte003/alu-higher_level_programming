ebb88e7fce5ce23d9fe06882333b8ab52b065075
Bonaparte003 <b.uteramaho@alustudent.com>
2024-07-29 02:19:24 -0700

    7-error_code.py

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""sends a request"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    code = req.code
+    if code >= 400:
+        print(f"Error code: {code}")
+    else:
+        print(req.text)
