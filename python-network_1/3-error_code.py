ae6dfe39a9cf23d2215237371afc482e8a538ebd
Bonaparte <b.uteramaho@alustudent.com>
2024-07-29 10:22:43 +0200

    3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-"""
-In takes a url, performs a request
-and handles errors if any
-"""
+"""scipt that intakes the url"""
 
-import urllib.request
-import urllib.error
-import sys
 
-if __name__ == '__main__':
-    """"STARTING THE PROCESS"""
-    url = sys.argv[1]
-    re = urllib.request.Request(url)
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
     try:
-        with urllib.request.urlopen(re) as response:
-            content = response.read()
-            print("{}".format(content.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-    except urllib.error.URLError as e:
-        print(e.reason)
+        with request.urlopen(sys.argv[1]) as ans:
+            print(ans.read().decode('UTF-8'))
+    except error.HTTPError as e:
+        print('Error code:', e.code)
