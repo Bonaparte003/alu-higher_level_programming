264382ba6b2204ecdfbb1634bb415325d7fff744
Bonaparte <b.uteramaho@alustudent.com>
2024-07-29 10:04:24 +0200

    updates

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -11,8 +11,9 @@ import urllib.request
 if __name__ == "__main__":
     """handles errors"""
     url = sys.argv[1]
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as f:
+        with urllib.request.urlopen(request) as f:
             print(f.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
