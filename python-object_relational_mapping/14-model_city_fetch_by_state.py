2cf942da649af1c3cc6f89b5905ea63de31cd1fa
Bonaparte003 <b.uteramaho@alustudent.com>
2024-07-31 09:21:39 -0700

    updates

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""contains all the modules in the ORM with a"""
-from sqlalchemy import asc, create_engine
+"""lists in the database """
 import sys
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 from model_city import City
 
-
 if __name__ == "__main__":
-    """connection"""
-    creator = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-    Base.metadata.create_all(creator)
-    Session = sessionmaker(bind=creator)
+    """sonnection"""
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    resul = session.query(City).order_by(City.id).all()
-    for i in resul:
-        st = session.query(State).filter(
-                State.id == i.state_id
-                ).one()
-        print(st.name + ": " + "(" + str(i.id) + ")" + i.name)
+
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
