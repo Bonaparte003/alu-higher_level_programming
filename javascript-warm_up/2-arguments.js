6361ee55b43e7105cf0c30019c8a683fa8700519
Bonaparte <b.uteramaho@alustudent.com>
2024-09-20 19:22:11 +0200

    typo errors corrections

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const a = process.argv.length -2 ;
+const a = process.argv.length - 2;
 if (a === 0) {
   console.log('No argument');
 } else if (a === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
