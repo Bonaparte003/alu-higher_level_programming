15c61952a054eb9546ff85162b2e6850350a7ac5
Bonaparte <b.uteramaho@alustudent.com>
2024-09-20 18:07:37 +0200

    javascriprt_warm_up

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv;
+let i = 0;
+if (args.length < 3) {
+    console.log('Missing number of occurrences');
+} else if (isNaN(parseInt(args[2]))) {
+    console.log('');
+} else {
+    while ( i < parseInt(args[2])) {
+        console.log("C is fun");
+        i++;
+    }
+}
