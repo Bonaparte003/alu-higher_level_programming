e79530be8a2c4e4fc3618ed7fe8ca19029f4ad27
Bonaparte <b.uteramaho@alustudent.com>
2024-10-03 21:07:17 +0200

    first attempt

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){
-    $.get("https://fourtonfish.com/hellosalut/?lang=fr", function(data){
-        console.log(data.hello);
-        $("DIV#hello").text(data.hello);
-    });
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+    method: 'GET',
+    success: function(data) {
+      $('#hello').text(data.hello);
+    },
+    error: function() {
+      console.error('An error occurred while fetching the data.');
+    }
+  });
 });
